f3211de7d1a8ebec012d2bfcc4ae0703b4303348
Mohit Sihag <mohitsihag7328@gmail.com>
2026-07-31 16:20:38 +0530

    done

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,23 +1,23 @@
 import { Hero } from '@/components/home/Hero';
-import { ValueProps } from '@/components/home/ValueProps';
+// import { ValueProps } from '@/components/home/ValueProps';
 import { Categories } from '@/components/home/Categories';
 import { ShopByType } from '@/components/home/ShopByType';
 import { FeaturedProducts } from '@/components/home/FeaturedProducts';
 import { FAQ } from '@/components/home/FAQ';
 import { Newsletter } from '@/components/home/Newsletter';
-// import { EthicalBadges } from '@/components/home/EthicalBadges';
+import { EthicalBadges } from '@/components/home/EthicalBadges';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <ValueProps />
+      {/* <ValueProps /> */}
       <ShopByType />
       <Categories />
       <FeaturedProducts />
       <FAQ />
       <Newsletter />
-      {/* <EthicalBadges /> */}
+      <EthicalBadges />
     </>
   );
 }
